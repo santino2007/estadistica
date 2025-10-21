5998145013ab07c9683d86a019a7f2e5b57d1ea1
santino2007 <santinobongiorno2007@gmail.com>
2025-10-21 18:55:40 -0300

    tkinter 2

--- codigo_3A.py
+++ codigo_3A.py
@@ -1,5 +1,5 @@
 
-from tkinter import Tk, Menu, Text, messagebox
+from tkinter import Tk, Menu, Text, messagebox, filedialog
 
 def on_click():
     messagebox.showinfo("information", "button clicked")
@@ -11,17 +11,28 @@ class Menuapp:
         self.text_area = Text(master, wrap='word', height=280, width=300)
         self.text_area.pack(side='left', fill='both', expand=True)
     
+    def abrir_archivo(self):
+        archivo = filedialog.askdirectory(initialdir="/", title="secciona un archivo")
+        if archivo: 
+            print(f"se abrio un archivo:{archivo}")
+            
+        else:
+            messagebox.INFO("no se pudo abrir el archivo")
+    
     def create_menu(self):
         barra_menu = Menu(self.master)
         self.master.config(menu=barra_menu)
 
         menu_cursos = Menu(barra_menu, tearoff=0)
         barra_menu.add_cascade(label="cursos", menu=menu_cursos)
-        menu_cursos.add_command(label="curso 1", command=on_click)
-        menu_cursos.add_command(label="curso 2", command=on_click)
-        menu_cursos.add_command(label="curso 3", command=on_click)
-        menu_cursos.add_command(label="curso 4", command=on_click)
-        menu_cursos.add_command(label="curso 5", command=on_click)
+        menu_cursos.add_command(label="3º A", command=self.abrir_archivo)
+        menu_cursos.add_command(label="3º B", command=on_click)
+        menu_cursos.add_command(label="3ª C", command=on_click)
+        menu_cursos.add_command(label="3º D", command=on_click)
+        menu_cursos.add_command(label="4º A", command=on_click)
+        menu_cursos.add_command(label="4º B", command=on_click)
+        
+        
         
         commands= [("salir", self.master.quit)]
         for (text, commands) in commands:
